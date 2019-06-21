89e3af1a858f698f7fee3f57eee49f363d8d9c03
Martin Fahy <mmartinfahy@gmail.com>
2019-06-21 14:02:23 -0700

    Parser class

--- kotlin/src/main/kotlin/icfp2019/Parsers.kt
+++ kotlin/src/main/kotlin/icfp2019/Parsers.kt
@@ -2,14 +2,20 @@ package icfp2019
 
 import com.google.common.base.CharMatcher
 
-val matcher = CharMatcher.anyOf("()")
-fun parsePoint(mapEdges: String): Point {
-  return parseEdges(mapEdges)[0]
-}
+class Parsers(expression: String) {
+  val matcher: CharMatcher
+  init {
+      matcher = CharMatcher.anyOf(expression)
+  }
 
-fun parseEdges(mapEdges: String): List<Point> {
-  return mapEdges.split(',')
-      .map { matcher.trimFrom(it) }
-      .windowed(2)
-      .map { Point(Integer.parseInt(it[0]), Integer.parseInt(it[1])) }
+  fun parsePoint(mapEdges: String): Point {
+    return parseEdges(mapEdges)[0]
+  }
+
+  fun parseEdges(mapEdges: String): List<Point> {
+    return mapEdges.split(',')
+            .map { icfp2019.matcher.trimFrom(it) }
+            .windowed(2)
+            .map { Point(Integer.parseInt(it[0]), Integer.parseInt(it[1])) }
+  }
 }
