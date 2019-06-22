98cac87749d1646bfb7961f30f1b330f02a2cee7
John Kercheval <jkercheval@godaddy.com>
2019-06-21 23:46:54 -0700

    Spotless crap

--- src/main/kotlin/icfp2019/app.kt
+++ src/main/kotlin/icfp2019/app.kt
@@ -2,7 +2,7 @@ package icfp2019
 
 import java.io.File
 
-fun main(args : Array<String>) {
+fun main(args: Array<String>) {
     File(args[0]).walk().forEach {
         if (it.isFile && it.extension.equals("desc")) {
             println("Running " + it.name)
@@ -15,11 +15,10 @@ fun main(args : Array<String>) {
 
 fun solve(problem: Problem): String {
 //    return Solution(problem)
-    println(problem);
-    return "Foo";
+    println(problem)
+    return "Foo"
 }
 
 fun constructObstacleMap(problem: Problem): Array<Array<Boolean>> {
     return problem.map.map { it.map { it.isObstacle }.toTypedArray() }.toTypedArray()
 }
-
