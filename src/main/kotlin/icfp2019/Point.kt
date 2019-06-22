f6f6c15722bff205e249003cce7da07a1b5aab6b
Jake <jake@jakeswenson.com>
2019-06-22 14:25:45 -0700

    graphz  ftw

--- src/main/kotlin/icfp2019/Point.kt
+++ src/main/kotlin/icfp2019/Point.kt
@@ -1,3 +1,32 @@
 package icfp2019
 
-data class Point(val x: Int, val y: Int)
+data class Point(val x: Int, val y: Int) {
+    fun isNeighbor(otherPoint: Point): Boolean {
+        return listOf(
+            Point(x - 1, y),
+            Point(x + 1, y),
+            Point(x, y - 1),
+            Point(x, y + 1),
+            Point(x - 1, y - 1),
+            Point(x - 1, y + 1),
+            Point(x + 1, y - 1),
+            Point(x + 1, y + 1)
+        ).contains(otherPoint)
+    }
+
+    fun up(): Point {
+        return copy(y = y + 1)
+    }
+
+    fun down(): Point {
+        return copy(y = y - 1)
+    }
+
+    fun left(): Point {
+        return copy(x = x - 1)
+    }
+
+    fun right(): Point {
+        return copy(x = x + 1)
+    }
+}
