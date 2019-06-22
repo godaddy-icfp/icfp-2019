73c6b6d2147b9628fd16615d679b6bebdf119344
Martin Fahy <mmartinfahy@gmail.com>
2019-06-21 21:35:25 -0700

    Save Game State to debug travis

--- src/main/kotlin/icfp2019/states/game/GameState.kt
+++ src/main/kotlin/icfp2019/states/game/GameState.kt
@@ -2,11 +2,18 @@ package icfp2019.states.game
 
 import icfp2019.Boosters
 import icfp2019.Point
+import icfp2019.Problem
 import icfp2019.objects.robot.RobotState
 
-data class GameState(
-  val gameBoard: Array<Short>, // TODO: Martin will replace with GameBoard Builder
-  val robotStateList: List<RobotState>,
-  val teleportDestination: List<Point>,
-  val unusedBoosters: List<Boosters>
-):w
+//class GameState(
+//  val gameBoard: Array<Short>, // TODO: Martin will replace with GameBoard Builder
+//  val robotStateList: List<RobotState>,
+//  val teleportDestination: List<Point>,
+//  val unusedBoosters: List<Boosters>
+//):w
+
+class GameState(problem: Problem) {
+  public val gameBoard: Array<Short> {
+    
+  }
+}
