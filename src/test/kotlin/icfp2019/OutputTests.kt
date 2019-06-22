5b7d9f77da4f8c6c79d6132f5a3d03127a7861ee
Cam Mach <cmach@godaddy.com>
2019-06-22 14:05:32 -0700

    clean up/rewrite the tests by Nathan suggestion

--- src/test/kotlin/icfp2019/OutputTests.kt
+++ src/test/kotlin/icfp2019/OutputTests.kt
@@ -4,19 +4,20 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class OutputTests {
+    fun getListOfAction() : MutableList<Action> {
+        return mutableListOf(
+            Action.MoveDown,
+            Action.MoveDown,
+            Action.MoveLeft,
+            Action.MoveUp,
+            Action.MoveUp,
+            Action.MoveRight)
+    }
+
     @Test
     fun testOutputOfOneRobot() {
-        val actions = mutableListOf<Action>()
-        actions.add(Action.MoveDown)
-        actions.add(Action.MoveDown)
-        actions.add(Action.MoveLeft)
-        actions.add(Action.MoveUp)
-        actions.add(Action.MoveUp)
-        actions.add(Action.MoveRight)
-
-        val map = mutableMapOf<RobotId, List<Action>>()
-        map.put(RobotId(1), actions)
-
+        val actions = getListOfAction()
+        val map = mapOf(RobotId(1) to actions)
         val actualOutput = Output.encodeRobotActions(map)
 
         Assertions.assertEquals("SSAWWD", actualOutput)
@@ -24,38 +25,27 @@ class OutputTests {
 
     @Test
     fun testOutputOfTwoRobot() {
-        val actions = mutableListOf<Action>()
-        actions.add(Action.MoveDown)
-        actions.add(Action.MoveDown)
-        actions.add(Action.MoveLeft)
-        actions.add(Action.MoveUp)
-        actions.add(Action.MoveUp)
-        actions.add(Action.MoveRight)
-
+        val actions = getListOfAction()
         // Adding two robots
-        val map = mutableMapOf<RobotId, List<Action>>()
-        map.put(RobotId(1), actions)
-        map.put(RobotId(2), actions)
-
+        val map = mapOf(
+            RobotId(1) to actions,
+            RobotId(2) to actions
+        )
         val actualOutput = Output.encodeRobotActions(map)
+
         Assertions.assertEquals("SSAWWD#SSAWWD", actualOutput)
     }
 
     @Test
     fun testOutputOfSolutionWithTeleport() {
-        val actions = mutableListOf<Action>()
-        actions.add(Action.MoveDown)
-        actions.add(Action.MoveDown)
-        actions.add(Action.MoveLeft)
-        actions.add(Action.MoveUp)
-        actions.add(Action.MoveUp)
-        actions.add(Action.MoveRight)
+        val actions = getListOfAction()
         actions.add(Action.TeleportBack(Point(1, 2)))
 
         // Adding two robots
-        val map = mutableMapOf<RobotId, List<Action>>()
-        map.put(RobotId(1), actions)
-        map.put(RobotId(2), actions)
+        val map = mapOf(
+            RobotId(1) to actions,
+            RobotId(2) to actions
+        )
 
         val actualOutput = Output.encodeRobotActions(map)
         Assertions.assertEquals("SSAWWDT(1,2)#SSAWWDT(1,2)", actualOutput)
@@ -63,19 +53,14 @@ class OutputTests {
 
     @Test
     fun testOutputOfSolutionWithManipulator() {
-        val actions = mutableListOf<Action>()
-        actions.add(Action.MoveDown)
-        actions.add(Action.MoveDown)
-        actions.add(Action.MoveLeft)
-        actions.add(Action.MoveUp)
-        actions.add(Action.MoveUp)
-        actions.add(Action.MoveRight)
+        val actions = getListOfAction()
         actions.add(Action.AttachManipulator(Point(1, 2)))
 
         // Adding two robots
-        val map = mutableMapOf<RobotId, List<Action>>()
-        map.put(RobotId(1), actions)
-        map.put(RobotId(2), actions)
+        val map = mapOf(
+            RobotId(1) to actions,
+            RobotId(2) to actions
+        )
 
         val actualOutput = Output.encodeRobotActions(map)
         Assertions.assertEquals("SSAWWDA(1,2)#SSAWWDA(1,2)", actualOutput)
