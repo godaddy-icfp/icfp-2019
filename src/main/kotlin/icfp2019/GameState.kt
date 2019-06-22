7bfdb7a91077be2a510cbc0f631c98374a39bcd3
John Kercheval <jkercheval@godaddy.com>
2019-06-22 11:46:49 -0700

    Correct point logic

--- src/main/kotlin/icfp2019/GameState.kt
+++ src/main/kotlin/icfp2019/GameState.kt
@@ -10,31 +10,29 @@ data class GameState(
     fun availableMoves(point: Point): List<Action> {
         val moves = mutableListOf<Action>()
 
-        val currentPosition = robotStateList[0].currentPosition
-
-        if (currentPosition.y + 1 < gameBoard.height) {
-            val up = gameBoard.get(currentPosition.x, currentPosition.y + 1)
+        if (point.y + 1 < gameBoard.height) {
+            val up = gameBoard.get(point.x, point.y + 1)
             if (!Cell.hasFlag(up, Cell.WRAPPED) && !Cell.hasFlag(up, Cell.OBSTACLE)) {
                 moves.add(Action.MoveUp)
             }
         }
 
-        if (currentPosition.y - 1 > -1) {
-            val down = gameBoard.get(currentPosition.x, currentPosition.y - 1)
+        if (point.y - 1 > -1) {
+            val down = gameBoard.get(point.x, point.y - 1)
             if (!Cell.hasFlag(down, Cell.WRAPPED) && !Cell.hasFlag(down, Cell.OBSTACLE)) {
                 moves.add(Action.MoveDown)
             }
         }
 
-        if (currentPosition.x - 1 > -1) {
-            val left = gameBoard.get(currentPosition.x - 1, currentPosition.y)
+        if (point.x - 1 > -1) {
+            val left = gameBoard.get(point.x - 1, point.y)
             if (!Cell.hasFlag(left, Cell.WRAPPED) && !Cell.hasFlag(left, Cell.OBSTACLE)) {
                 moves.add(Action.MoveLeft)
             }
         }
 
-        if (currentPosition.x + 1 < gameBoard.width) {
-            val right = gameBoard.get(currentPosition.x + 1, currentPosition.y)
+        if (point.x + 1 < gameBoard.width) {
+            val right = gameBoard.get(point.x + 1, point.y)
             if (!Cell.hasFlag(right, Cell.WRAPPED) && !Cell.hasFlag(right, Cell.OBSTACLE)) {
                 moves.add(Action.MoveRight)
             }
