8562637ebac20c6fc4618f32fa913500974ee87d
Martin Fahy <mmartinfahy@gmail.com>
2019-06-21 22:30:16 -0700

    Getter

--- src/main/kotlin/icfp2019/objects/gameboard/Gameboard.kt
+++ src/main/kotlin/icfp2019/objects/gameboard/Gameboard.kt
@@ -3,29 +3,30 @@ package icfp2019.objects.gameboard
 import icfp2019.Node
 import icfp2019.Point
 import icfp2019.Problem
+import java.lang.IllegalStateException
 import java.util.stream.Stream
 
 class GameBoard(_problem: Problem) {
-  val problem : Problem
+  private val problem : Problem
   init {
     this.problem = _problem
   }
 
+  private fun failure(message: String): Nothing {
+    throw IllegalStateException(message)
+  }
+
   // Used for Flattening Problem 2D array -> 1D of shorts
-  val cells: List<Node> = listOf<Node>()
-    get() = this.problem.map.flatten()
+  val cells: List<Node>
     get() {
       val oneDimenMap = this.problem.map.flatten()
       when(oneDimenMap.isNotEmpty()) {
         true -> return oneDimenMap
-        false -> throw IllegalStateExcepton("Could not initialize cells with input ${this.problem}")
+        false -> failure("Could not initialize cells with input ${this.problem}")
       }
     }
-    set(value) {
-      when(value.isNotEmpty()) {
-        true -> field = value
-        false -> throw IllegalStateException("Incorrect input value ${value}")
-      }
-    }
+
+
+
 
 }
