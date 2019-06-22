42700aa58f54d772ffaa4729625dc60425ecb241
aiyangar <aiyangar@godaddy.com>
2019-06-21 20:29:30 -0700

    fix spotless.

--- src/main/kotlin/icfp2019/app.kt
+++ src/main/kotlin/icfp2019/app.kt
@@ -87,13 +87,14 @@ enum class Actions {
 data class RobotState(
     val robotId: RobotId,
     val currentPosition: Point,
-    val orientation: Orientation=Orientation.Up,
-    val remainingFastWheelTime: Int?=null,
-    val remainingDrillTime: Int?=null)
+    val orientation: Orientation = Orientation.Up,
+    val remainingFastWheelTime: Int? = null,
+    val remainingDrillTime: Int? = null
+)
 
 data class RobotId(val id: Int)
 
-enum class Orientation{
+enum class Orientation {
     Up, Down, Left, Right
 }
 
