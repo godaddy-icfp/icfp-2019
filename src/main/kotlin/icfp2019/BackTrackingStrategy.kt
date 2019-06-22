a2e55be95e62a7b1e74fde0047d49edd5517acbc
Jason Mackay <jmackay@godaddy.com>
2019-06-22 01:25:48 -0700

    DFS backtracking strategy + gameboard stubs

--- src/main/kotlin/icfp2019/BackTrackingStrategy.kt
+++ src/main/kotlin/icfp2019/BackTrackingStrategy.kt
@@ -1,18 +1,62 @@
 package icfp2019
 
+import java.lang.IllegalStateException
+import java.util.*
+
+class TransitionEngine {
+    fun apply(gameState: GameState, actions: Map<Int, Action>): GameState {
+        // TODO: implement later
+
+        actions.forEach { println("yay!" + it.key.toString()) }
+
+        return GameState(gameState.gameBoard, listOf(), listOf(), listOf())
+    }
+}
+
 // Move to an open space and push moves onto a stack, if no moves available then backtrack using the stack
 class BackTrackingStrategy : Strategy {
     override fun getActions(gameState: GameState): Map<Int, List<Action>> {
-        // initialize robot stacks
-        // initialize robot move lists
+        val stack = Stack<Action>()
+        val moveList = mutableListOf<Action>()
+        val tEngine = TransitionEngine()
 
-        // for each robot:
-            // get available squares
-            // if no available squares
-                // if robot stack is empty
-                    // continue, robot is done
-                // else
-                    // pop from stack, apply inverse action
-        return HashMap<Int, List<Action>>()
+        var currentState = gameState
+        var availableMoves = availableMoves(gameState)
+        do {
+            if (availableMoves.isEmpty()) {
+                // Backtrack if we can
+                if (stack.isEmpty()) {
+                    break
+                } else {
+                    val backTrackAction = stack.pop().invert() ?: throw IllegalStateException("Non-invertable action!")
+
+                    currentState = tEngine.apply(currentState, hashMapOf(Pair<Int, Action>(0, backTrackAction)))
+                    stack.push(backTrackAction)
+                    moveList.add(backTrackAction)
+                    availableMoves = availableMoves(gameState)
+                }
+            } else {
+                // Else take the first available move
+                val action = availableMoves.get(0)
+                currentState = tEngine.apply(currentState, hashMapOf(Pair<Int, Action>(0, action)))
+                stack.push(action)
+                moveList.add(action)
+                availableMoves = availableMoves(gameState)
+            }
+        } while (stack.isNotEmpty() || availableMoves.isNotEmpty())
+
+        val plans = hashMapOf<Int, List<Action>>(
+            Pair<Int, List<Action>>(0, moveList))
+
+        return plans
+    }
+
+    fun availableMoves(gameState: GameState): List<Action> {
+        // TODO: Implemenet available moves
+        if (gameState.gameBoard.isNotEmpty()) {
+            print("hi!")
+        }
+
+        return listOf<Action>()
     }
 }
