d828e53ab75fbaefd23b7c86b11c231bf637a42f
aiyangar <aiyangar@godaddy.com>
2019-06-22 15:38:24 -0700

    change to start using analyzer.

---
+++ src/main/kotlin/icfp2019/ShortestPathUsingDijkstra.kt
@@ -0,0 +1,17 @@
+package icfp2019
+
+import icfp2019.analyzers.GraphAnalyzer
+import icfp2019.core.Analyzer
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath
+import org.jgrapht.graph.DefaultEdge
+
+object ShortestPathUsingDijkstra : Analyzer<ShortestPathAlgorithm<Node, DefaultEdge>> {
+    override fun analyze(map: GameBoard): (state: GameState) -> ShortestPathAlgorithm<Node, DefaultEdge> {
+        val completeGraph = GraphAnalyzer.analyze(map)
+        return {graphState ->
+            val graph = completeGraph(graphState)
+            DijkstraShortestPath(graph)
+        }
+    }
+}
