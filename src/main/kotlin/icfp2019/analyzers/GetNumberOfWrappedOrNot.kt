30b49b404d98905a23abdbf88c855b3ecab9c2ea
tim <teemobean@gmail.com>
2019-06-22 13:51:09 -0700

    Add wrapped or not

--- src/main/kotlin/icfp2019/analyzers/GetNumberOfWrappedOrNot.kt
+++ src/main/kotlin/icfp2019/analyzers/GetNumberOfWrappedOrNot.kt
@@ -1,5 +1,6 @@
 package icfp2019.analyzers
 
+import icfp2019.Cell
 import icfp2019.GameBoard
 import icfp2019.GameState
 import icfp2019.core.Analyzer
@@ -9,7 +10,14 @@ data class WrappedUnwrapped(val wrapped: Int, val unwrapped: Int)
 class GetNumberOfWrappedOrNot : Analyzer<WrappedUnwrapped> {
     override fun analyze(map: GameBoard): (state: GameState) -> WrappedUnwrapped {
         return { _ ->
-            WrappedUnwrapped(0, 0)
+            map.rows().flatten().map {
+                if (Cell.hasFlag(it, Cell.WRAPPED)) {
+                    WrappedUnwrapped(1, 0)
+                } else {
+                    WrappedUnwrapped(0, 1)
+                }
+            }
+                .reduce { a, b -> WrappedUnwrapped(a.wrapped + b.wrapped, a.unwrapped + b.unwrapped) }
         }
     }
 }
---
+++ src/test/kotlin/icfp2019/GetNumberOfWrappedOrNotTests.kt
@@ -0,0 +1,10 @@
+package icfp2019
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class GetNumberOfWrappedOrNotTests {
+    @Test
+    fun testSimple() {
+    }
+}
