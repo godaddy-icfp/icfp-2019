034a0a68f6c424d7f6da03c5ef2d7d73bfb57639
Martin Fahy <mmartinfahy@gmail.com>
2019-06-21 20:59:29 -0700

    Stable till merge

---
+++ src/main/kotlin/icfp2019/actions/Actions.kt
@@ -0,0 +1,5 @@
+package icfp2019.actions
+
+enum class Actions {
+  W, S, A, D, Z, E, Q, B, F, L
+}
--- src/main/kotlin/icfp2019/transaction/TransitionEngine.kt
+++ src/main/kotlin/icfp2019/transaction/TransitionEngine.kt
@@ -1,10 +1,15 @@
 package icfp2019.transaction
 
 import com.google.common.collect.ImmutableMap
+import icfp2019.Actions
+import icfp2019.RobotId
+
+
+class GameState {}
 
 class TransitionEngine {
   // -> Return GameState
-  fun apply(currentGameState: GameState, robotIdMap: ImmutableMap<RobotId, Action>): Nothing {
+  fun apply(currentGameState: GameState, robotIdMap: ImmutableMap<RobotId, Actions>): Nothing {
     // Implement meaningful shit here
     TODO("Implement apply")
   }
