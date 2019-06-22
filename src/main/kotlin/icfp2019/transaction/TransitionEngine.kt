ba9f80d830a477a89061337581c2a36f3875ddea
Martin Fahy <mmartinfahy@gmail.com>
2019-06-21 21:08:41 -0700

    Get Game State from latest merge

--- src/main/kotlin/icfp2019/transaction/TransitionEngine.kt
+++ src/main/kotlin/icfp2019/transaction/TransitionEngine.kt
@@ -2,10 +2,10 @@ package icfp2019.transaction
 
 import com.google.common.collect.ImmutableMap
 import icfp2019.Actions
+import icfp2019.GameState
 import icfp2019.RobotId
 
 
-class GameState {}
 
 class TransitionEngine {
   // -> Return GameState
