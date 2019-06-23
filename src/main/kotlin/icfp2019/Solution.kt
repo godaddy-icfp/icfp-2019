e29ca1ea5c1fb4b0f1f3ae559ac121eea9df3939
John Kercheval <jkercheval@godaddy.com>
2019-06-23 13:47:25 -0700

    spotless

--- src/main/kotlin/icfp2019/Solution.kt
+++ src/main/kotlin/icfp2019/Solution.kt
@@ -6,13 +6,14 @@ import icfp2019.model.RobotId
 
 data class Solution(
     val problem: Problem,
-    val actions: Map<RobotId, List<Action>>) {
+    val actions: Map<RobotId, List<Action>>
+) {
 
     override fun toString(): String {
         return actions.encodeActions()
     }
 
-    fun summary () : String {
+    fun summary(): String {
         return "Size: ${problem.size.x}x${problem.size.y} solved in ${actions.size} steps"
     }
 }
