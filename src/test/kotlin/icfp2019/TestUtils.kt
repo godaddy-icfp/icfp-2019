f920dde346d00c0b00c156a770813a0000efecda
Jake <jake@jakeswenson.com>
2019-06-22 21:12:42 -0700

    fix build

--- src/test/kotlin/icfp2019/TestUtils.kt
+++ src/test/kotlin/icfp2019/TestUtils.kt
@@ -1,8 +1,12 @@
 package icfp2019
 
+import com.google.common.base.CharMatcher
+import com.google.common.base.Splitter
+import icfp2019.model.MapSize
 import icfp2019.model.Node
 import icfp2019.model.Point
 import icfp2019.model.Problem
+import org.pcollections.TreePVector
 import java.nio.file.Paths
 
 fun loadProblem(problemNumber: Int): String {
@@ -34,4 +38,31 @@ fun printBoard(p: Problem, path: Set<Node> = setOf()) {
     println("${p.size}")
     print(boardString(p, path))
     println()
+}
+
+fun parseTestMap(map: String): Problem {
+    val mapLineSplitter = Splitter.on(CharMatcher.anyOf("\r\n")).omitEmptyStrings()
+    val lines = mapLineSplitter.splitToList(map).map { CharMatcher.whitespace().removeFrom(it) }.reversed()
+    val height = lines.size
+    val width = lines[0].length
+    if (lines.any { it.length != width }) throw IllegalArgumentException("Inconsistent map line lengths")
+    val startPoint =
+        (0 until width).map { x ->
+            (0 until height).map { y ->
+                if (lines[y][x] == '@') Point(x, y)
+                else null
+            }
+        }.flatten().find { it != null } ?: Point.origin()
+    return Problem(MapSize(width, height), startPoint, TreePVector.from((0 until width).map { y ->
+        TreePVector.from((0 until height).map { x ->
+            val point = Point(x, y)
+            when (lines[x][y]) {
+                'X' -> Node(point, isObstacle = true)
+                'w' -> Node(point, isObstacle = false, isWrapped = true)
+                '.' -> Node(point, isObstacle = false)
+                '@' -> Node(point, isObstacle = false)
+                else -> throw IllegalArgumentException("Unknown Char '${lines[x][y]}'")
+            }
+        })
+    }))
 }
