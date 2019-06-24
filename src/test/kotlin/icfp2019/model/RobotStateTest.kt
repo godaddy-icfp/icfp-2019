a813edc374e361cad01edead91b8b4b98f3d3307
John Kercheval <jkercheval@godaddy.com>
2019-06-23 21:14:50 -0700

    Finish merge for manipulator apply

--- src/test/kotlin/icfp2019/model/RobotStateTest.kt
+++ src/test/kotlin/icfp2019/model/RobotStateTest.kt
@@ -1,6 +1,5 @@
 package icfp2019.model
 
-import icfp2019.core.ActionStateTransitionEngineKtTests
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -10,10 +9,10 @@ internal class RobotStateTest {
 
     @Test
     fun optimumManipulatorArmTarget() {
-        var state = RobotState(RobotId(1), Point(0, 0))
+        var state = RobotState(RobotId.first, Point(0, 0))
 
-        Assertions.assertEquals(Point(1,2), state.optimumManipulatorArmTarget())
-        state = state.copy(RobotId(2), armRelativePoints = state.armRelativePoints.plus(Point(1,2)))
-        Assertions.assertEquals(Point(1,-2), state.optimumManipulatorArmTarget())
+        Assertions.assertEquals(Point(1, 2), state.optimumManipulatorArmTarget())
+        state = state.copy(RobotId.first, armRelativePoints = state.armRelativePoints.plus(Point(1, 2)))
+        Assertions.assertEquals(Point(1, -2), state.optimumManipulatorArmTarget())
     }
 }
